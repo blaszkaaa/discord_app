ccce47c5eb3f2fde4e84b73a3c60c44f203ea231
blaszkaaa <58654924+blaszkaaa@users.noreply.github.com>
2022-10-06 22:12:33 +0200

    1

--- lib/api/apiPost.dart
+++ lib/api/apiPost.dart
@@ -20,7 +20,7 @@ class _apiPostCallState extends State<apiPostCall> {
       Uri.parse('https://discord.com/api/v9/channels/1027584170463068213/messages'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
-        HttpHeaders.authorizationHeader: 'NzkyNTA0NDc2MzE3Mzg0Nzg0.GuAMkL.xToGGZOYKpW5u3FQH-gTzlGrsvaW1WqFTd5Fk4',
+        HttpHeaders.authorizationHeader: 'NzkyNTA0NDc2MzE3Mzg0Nzg0.GGa_8a.cK0-Ky26WkV5Y2jGC7fjmOqGD3Z7LfgbxEZXHQ',
       },
       body: jsonEncode(<String, String>{
         "content": result,
